bd7504ed23e62eeefb56524a8fe2b7550783ec7e
kristina2331 <sapovalovakristina0@gmail.com>
2026-09-17 18:13:26 +0300

    Lab01:variables and interpolation

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string myName = "Кристина Шаповалова";
+string groupName = "ИСП253";
+int courseNumber = 2;
+double averageGrade = 4.6;
+bool isBudget = true;
+
+Console.WriteLine("Знакомство");
+Console.WriteLine($"Студент: {myName}");
+Console.WriteLine($"Группа: {groupName}");
+Console.WriteLine($"Курс: {courseNumber}");
+Console.WriteLine($"Средний балл: {averageGrade}");
+Console.WriteLine($"Бюджетное место: {isBudget}");
+
